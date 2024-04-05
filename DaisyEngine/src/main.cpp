1a20d164c126b4ea04014fd8ce945ba42ad35f84
kaitabuchi <kaidanieltabuchi@gmail.com>
2024-04-04 19:23:23 -0500

    Cleanup Code and add Scale

--- DaisyEngine/src/main.cpp
+++ DaisyEngine/src/main.cpp
@@ -6,36 +6,30 @@
 #include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
 
-void DrawBox(double x, double y, double z, float angle, GLuint VBO, GLuint VAO, GLuint shaderProgram, GLuint texture, glm::mat4 projection, glm::mat4 view)
+void DrawBox(glm::vec3 position, glm::vec3 scale, float angle, GLuint VBO, GLuint VAO, GLuint EBO, GLuint shaderProgram, GLuint texture, glm::mat4 projection, glm::mat4 view)
 {
-    // Calculate model matrix (rotate cube)
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_VERTEX_ARRAY, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
     glm::mat4 model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(1.0f, 0.5f, 0.0f));
 
-    // Translation code
-    glm::vec3 translationVector(x, y, z); // Example translation vector (move along the x-axis)
-    model = glm::translate(model, translationVector);
+    model = glm::translate(model, position);
+    model = glm::scale(model, scale);
 
-    // Use shader program
     glUseProgram(shaderProgram);
-    // Set uniform matrices
+
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
-    // Bind texture
     glBindTexture(GL_TEXTURE_2D, texture);
 
-    // Bind VAO
-    glBindVertexArray(VAO);
-
-    // Draw cube
     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
-    // Unbind VAO
     glBindVertexArray(0);
 }
 
-// Vertex shader source code
 const char* vertexShaderSource =
 R"(
     #version 330 core
@@ -53,7 +47,6 @@ R"(
     }
 )";
 
-// Fragment shader source code
 const char* fragmentShaderSource =
 R"(
     #version 330 core
@@ -69,19 +62,17 @@ R"(
 
 int main()
 {
-    // Initialize GLFW
+
     if (!glfwInit())
     {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
-    // Set GLFW window hints
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Create a GLFW window
     GLFWwindow* window = glfwCreateWindow(1350, 900, "OpenGL Texture", nullptr, nullptr);
     if (!window)
     {
@@ -91,7 +82,6 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
-    // Initialize GLEW
     if (glewInit() != GLEW_OK)
     {
         std::cerr << "Failed to initialize GLEW" << std::endl;
@@ -101,12 +91,11 @@ int main()
     glEnable(GL_BLEND);
     glEnable(GL_DEPTH_TEST);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    // Compile vertex shader
+
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
 
-    // Check vertex shader compilation errors
     GLint success;
     GLchar infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -116,12 +105,10 @@ int main()
         std::cerr << "Vertex shader compilation failed: " << infoLog << std::endl;
     }
 
-    // Compile fragment shader
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
     glCompileShader(fragmentShader);
 
-    // Check fragment shader compilation errors
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success)
     {
@@ -129,13 +116,11 @@ int main()
         std::cerr << "Fragment shader compilation failed: " << infoLog << std::endl;
     }
 
-    // Create shader program
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
-    // Check shader program linking errors
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success)
     {
@@ -143,81 +128,72 @@ int main()
         std::cerr << "Shader program linking failed: " << infoLog << std::endl;
     }
 
-    // Delete shaders as they are linked into our program now and no longer necessary
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // Define the vertices and texture coordinates of the cube
     float vertices[] = {
-        // Front face
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // Bottom-left
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // Bottom-right
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // Top-right
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // Top-left
-        // Back face
-        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // Bottom-right
-         0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Bottom-left
-         0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // Top-left
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // Top-right
-        // Left face
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // Top-right
-        -0.5f,  0.5f, -0.5f,  0.0f, 0.0f, // Top-left
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // Bottom-left
-        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, // Bottom-right
-        // Right face
-         0.5f,  0.5f,  0.5f,  0.0f, 0.0f, // Top-left
-         0.5f,  0.5f, -0.5f,  1.0f, 0.0f, // Top-right
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // Bottom-right
-         0.5f, -0.5f,  0.5f,  0.0f, 1.0f, // Bottom-left
-         // Top face
-         -0.5f,  0.5f,  0.5f,   0.0f, 1.0f, // Bottom-left
-          0.5f,  0.5f,  0.5f,   1.0f, 1.0f, // Bottom-right
-          0.5f,  0.5f, -0.5f,   1.0f, 0.0f, // Top-right
-         -0.5f,  0.5f, -0.5f,   0.0f, 0.0f, // Top-left
-         // Bottom face
-         -0.5f, -0.5f,  0.5f,   1.0f, 1.0f, // Top-right
-          0.5f, -0.5f,  0.5f,   0.0f, 1.0f, // Top-left
-          0.5f, -0.5f, -0.5f,   0.0f, 0.0f, // Bottom-left
-         -0.5f, -0.5f, -0.5f,   1.0f, 0.0f  // Bottom-right
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f,
+
+         0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
+
+         -0.5f,  0.5f,  0.5f,   0.0f, 1.0f,
+          0.5f,  0.5f,  0.5f,   1.0f, 1.0f,
+          0.5f,  0.5f, -0.5f,   1.0f, 0.0f,
+         -0.5f,  0.5f, -0.5f,   0.0f, 0.0f,
+
+         -0.5f, -0.5f,  0.5f,   1.0f, 1.0f,
+          0.5f, -0.5f,  0.5f,   0.0f, 1.0f,
+          0.5f, -0.5f, -0.5f,   0.0f, 0.0f,
+         -0.5f, -0.5f, -0.5f,   1.0f, 0.0f
     };
 
     unsigned int indices[] = {
-        0,  1,  2,  2,  3,  0,   // Front face
-        4,  5,  6,  6,  7,  4,   // Back face
-        8,  9, 10, 10, 11,  8,   // Left face
-        12, 13, 14, 14, 15, 12,  // Right face
-        16, 17, 18, 18, 19, 16,  // Top face
-        20, 21, 22, 22, 23, 20   // Bottom face
+        0,  1,  2,  2,  3,  0,
+        4,  5,  6,  6,  7,  4,
+        8,  9, 10, 10, 11,  8,
+        12, 13, 14, 14, 15, 12,
+        16, 17, 18, 18, 19, 16,
+        20, 21, 22, 22, 23, 20
     };
 
-    // Create Vertex Buffer Object (VBO) and Element Buffer Object (EBO) to store the vertices and indices
     GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
-    // Bind VAO
     glBindVertexArray(VAO);
 
-    // Bind VBO and set vertex data
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Bind EBO and set index data
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Set vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // Unbind VBO and VAO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    // Load image using stb_image
     stbi_set_flip_vertically_on_load(true);
 
     int width, height, nrChannels;
@@ -228,18 +204,15 @@ int main()
         return -1;
     }
 
-    // Generate and bind texture
     GLuint texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
 
-    // Set texture parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    // Load image data into texture
     if (nrChannels == 3)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -250,21 +223,17 @@ int main()
     }
     glGenerateMipmap(GL_TEXTURE_2D);
 
-    // Free image data
     stbi_image_free(data);
 
-    // Define projection matrix
     int w, h;
     glfwGetWindowSize(window, &w, &h);
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)w / (float)h, 0.1f, 100.0f);
-    // Define view matrix
-    //glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
     glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
     glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
     glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
     float cameraSpeed = 0.0008f;
 
-    // Rendering loop
     while (!glfwWindowShouldClose(window))
     {
         if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
@@ -290,28 +259,23 @@ int main()
         if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
             cameraFront = glm::mat3(glm::rotate(glm::mat4(1.0f), glm::radians(-cameraRotSpeed), glm::normalize(glm::cross(cameraFront, cameraUp)))) * cameraFront;
 
-        // Update view matrix
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-        // Render
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        DrawBox(1, 0, 1, 100, VBO, VAO, shaderProgram, texture, projection, view);
+        DrawBox(glm::vec3(0, 0, 0), glm::vec3(1, 1, 1), 100, VBO, VAO, EBO, shaderProgram, texture, projection, view);
 
-        DrawBox(0, 0, 0, 0, VBO, VAO, shaderProgram, texture, projection, view);
-        // Swap buffers and poll IO events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Delete resources
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
     glDeleteTextures(1, &texture);
     glDeleteProgram(shaderProgram);
 
-    // Terminate GLFW
     glfwTerminate();
     return 0;
 }
