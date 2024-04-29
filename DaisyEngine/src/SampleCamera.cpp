3b860247e8896bbff5dff620baab0c2dc6c3a4cb
kaitabuchi <kaidanieltabuchi@gmail.com>
2024-04-29 18:36:42 -0500

    Revert "Revert "Add 2D Renderer""
    This reverts commit 7865e73e8b53c74a2c431802064eb04c1c0c282c.

--- DaisyEngine/src/SampleCamera.cpp
+++ DaisyEngine/src/SampleCamera.cpp
@@ -31,5 +31,22 @@ namespace Daisy
             camera->cameraFront = glm::mat3(glm::rotate(glm::mat4(1.0f), glm::radians(-cameraRotSpeed), glm::normalize(glm::cross(camera->cameraFront, camera->cameraUp)))) * camera->cameraFront;
     }
 
-    
+    void SampleMoveCamera2D(Daisy::Camera* camera, Daisy::Window window)
+    {
+        float cameraSpeed = 0.0008f;
+        float cameraRotSpeed = 0.025f;
+
+        if (KeyInput::GetKeyDown(window, DAISY_KEY_W))
+            camera->cameraPos += cameraSpeed * camera->cameraFront;
+        if (KeyInput::GetKeyDown(window, DAISY_KEY_S))
+            camera->cameraPos -= cameraSpeed * camera->cameraFront;
+        if (KeyInput::GetKeyDown(window, DAISY_KEY_Q))
+            camera->cameraPos += cameraSpeed * camera->cameraUp;
+        if (KeyInput::GetKeyDown(window, DAISY_KEY_E))
+            camera->cameraPos -= cameraSpeed * camera->cameraUp;
+        if (KeyInput::GetKeyDown(window, DAISY_KEY_A))
+            camera->cameraPos -= glm::normalize(glm::cross(camera->cameraFront, camera->cameraUp)) * cameraSpeed;
+        if (KeyInput::GetKeyDown(window, DAISY_KEY_D))
+            camera->cameraPos += glm::normalize(glm::cross(camera->cameraFront, camera->cameraUp)) * cameraSpeed;
+    }
 }
